92fe6547b784486b415533c2c16b05655b80ed8f
alex melboourne <alex melboourne>
2017-01-23 23:12:56 +0000

    Added in more jquery animations

--- js/directives.js
+++ js/directives.js
@@ -8,10 +8,11 @@ angular.module('directives', [])
         $('.card').click (function () {
           $(this).addClass('thisactivecard'), 10;
           $(this).find('.card__bannerPreview--bannerImg').animate({height: '70px', width: '22%', borderRadius: '200px', margin: '0 39%', position: 'relative', top: '30px'},{ duration: 10, queue: false });
-          $(this).find('.card__bannerPreview--heading').animate({top: '-150px'});
+          $(this).find('.card__bannerPreview--heading').animate({top: '-150px'}, { duration: 10, queue: false });
           $(this).find('.card__bannerPreview--subheading').animate({top: '0', marginBottom: '0'}, { duration: 10, queue: false });
           $(this).find('.card__bannerPreview--body').animate({top: '10px'}, { duration: 10, queue: false });
           $(this).find('.card__bannerPreviewFooter').animate({top: '10px'}, { duration: 10, queue: false });
+          $(this).find('.closepls').fadeIn(), 10;
         })
       }
     }
@@ -23,9 +24,14 @@ angular.module('directives', [])
     replace: true,
     template: '<div class="closepls"><i class="fa fa-times" aria-hidden="true"></i></div>',
     link:function(scope, element, attribute) {
-      $('.closepls').click(function () {
-        $('.card').removeClass('thisactivecard'), 20;
-        $('.card__bannerPreview--bannerImg').animate({height: '160px', width: '100%', borderRadius: '0px', margin: '0', position: 'relative', top: '0'},{ duration: 10, queue: false });
+      $('.closepls').click(function (e, event) {
+        e.stopPropagation();
+        $(this).parent().find('.card__bannerPreview--heading').animate({top: '20px'}, { duration: 10, queue: false });
+        $(this).parent().find('.card__bannerPreview--body').animate({top: '150px'}, { duration: 10, queue: false });
+        $(this).parent().find('.card__bannerPreview--subheading').animate({top: '150px'}, { duration: 10, queue: false });
+        $(this).parent().find('.card__bannerPreview--bannerImg').animate({height: '160px', width: '100%', borderRadius: '0px', margin: '0', position: 'relative', top: '0'},{ duration: 10, queue: false });
+        $(this).closest('.card').removeClass('thisactivecard'), 20;
+        $(this).fadeOut(), 10;
       })
     }
   }
